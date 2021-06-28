e8e905e507d658fa73664324c348473811fc6f92
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-06-28 09:54:36 +0800

    q

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,7 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
-    'homeUrl' => '/admin',
+    
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
@@ -20,7 +20,6 @@ return [
                 'httpOnly' => true,
                 'path' => '/admin',
             ],
-            'baseUrl' => '/admin',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -51,14 +50,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        
+        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        
+        */
     ],
     'params' => $params,
 ];
