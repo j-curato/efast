1459966e80c3b2116d895795ebf9a75bb52a28cb
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-07-01 12:04:38 +0800

    qwer

--- frontend/web/js/scripts.js
+++ frontend/web/js/scripts.js
@@ -5,6 +5,7 @@ function thousands_separators(num) {
     num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return num_parts.join(".");
 }
+// GET ALL BOOKS
 
 $.getJSON('/dti-afms-2/frontend/web/index.php?r=books/get-books')
     .then(function (data) {
@@ -24,6 +25,8 @@ $.getJSON('/dti-afms-2/frontend/web/index.php?r=books/get-books')
         })
 
     });
+// GET TRANSACTIONs
+
 $.getJSON('/dti-afms-2/frontend/web/index.php?r=transaction/get-all-transaction')
     .then(function (data) {
 
@@ -42,3 +45,19 @@ $.getJSON('/dti-afms-2/frontend/web/index.php?r=transaction/get-all-transaction'
         })
 
     });
+
+// GET CHART OF ACCOUNTS
+$.getJSON('/dti-afms-2/frontend/web/index.php?r=chart-of-accounts/get-general-ledger')
+    .then(function (data) {
+        var array = []
+        $.each(data, function (key, val) {
+            array.push({
+                id: val.id,
+                text: val.object_code + ' ' + val.title
+            })
+        })
+        accounts = array
+        // var y=JSON.parse(accounts)
+        chart_of_accounts = data
+
+    })
