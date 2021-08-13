6dec6b2d1a9093e71125ad9cad50cbeb984d4384
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-08-13 14:25:17 +0800

    cibr loader

--- frontend/views/cibr/view.php
+++ frontend/views/cibr/view.php
@@ -39,157 +39,158 @@ $this->params['breadcrumbs'][] = $this->title;
 
         </div>
     </div>
+    <div id="con">
 
-    <?php Pjax::begin(['id' => 'cibr', 'clientOptions' => ['method' => 'POST']]) ?>
-    <table>
+        <?php Pjax::begin(['id' => 'cibr', 'clientOptions' => ['method' => 'POST']]) ?>
+        <table>
 
-        <thead>
-            <tr>
-                <th colspan="12" style="text-align: center;border:1px solid white">CASH IN BANK REGISTER</th>
-            </tr>
-            <tr>
-                <th colspan="12" style="text-align: center;border:1px solid white">
+            <thead>
+                <tr>
+                    <th colspan="12" style="text-align: center;border:1px solid white">CASH IN BANK REGISTER</th>
+                </tr>
+                <tr>
+                    <th colspan="12" style="text-align: center;border:1px solid white">
 
-                    <span>
-                        For the month of <?php
-                                            if (!empty($reporting_period)) {
-                                                echo date('F, Y', strtotime($reporting_period));
-                                            }
-                                            ?>
-                    </span>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="9" class="header">
-                    <span> Entity Name:Department of Trade and Industry</span>
-                </th>
-                <th colspan="3" class="header">
-                    <span>
-                        Sheet No. :_____________________
-                    </span>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="9" class="header">
-                    <span> Sub-Office/District/Division: Provincial Office</span>
-                </th>
-                <th colspan="3" class="header">
-                    <span>
-                        Name of Disbursing Officer: <?php
-
-                                                    if (!empty($province)) {
-                                                        // echo $prov[$province]['officer'];
-                                                        $prov = Yii::$app->memem->cibrCdrHeader($province);
-                                                        echo $prov['officer'];
-                                                        // echo Yii::$app->memem->cibrCdrHeader($province)['officer'];
-
-
-                                                    }
-                                                    ?>
-                    </span>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="9" class="header">
-                    <span> Municipality/City/Province: <?php
+                        <span>
+                            For the month of <?php
+                                                if (!empty($reporting_period)) {
+                                                    echo date('F, Y', strtotime($reporting_period));
+                                                }
+                                                ?>
+                        </span>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="9" class="header">
+                        <span> Entity Name:Department of Trade and Industry</span>
+                    </th>
+                    <th colspan="3" class="header">
+                        <span>
+                            Sheet No. :_____________________
+                        </span>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="9" class="header">
+                        <span> Sub-Office/District/Division: Provincial Office</span>
+                    </th>
+                    <th colspan="3" class="header">
+                        <span>
+                            Name of Disbursing Officer: <?php
 
                                                         if (!empty($province)) {
-                                                            // echo $prov[$province]['province'];
-                                                            // echo Yii::$app->memem->cibrCdrHeader($province)['province'];
-                                                            echo $prov['province'];
+                                                            // echo $prov[$province]['officer'];
+                                                            $prov = Yii::$app->memem->cibrCdrHeader($province);
+                                                            echo $prov['officer'];
+                                                            // echo Yii::$app->memem->cibrCdrHeader($province)['officer'];
+
+
                                                         }
-                                                        ?></span>
-                </th>
-                <th colspan="3" class="header">
-                    <span>
-                        Station: Surigao del Norte
-                    </span>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="9" rowspan="2" style="border-left:1px solid white;border-right:1px solid white;">
-                    <span> Fund Cluster : <?php
-                                            if (!empty($book)) {
+                                                        ?>
+                        </span>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="9" class="header">
+                        <span> Municipality/City/Province: <?php
 
-                                                echo $book;
-                                            }
-                                            ?></span>
-                </th>
-                <th colspan="3" class="header" style="border-left:1px solid white;border-right:1px solid white;">
-                    <span>
-                        Bank : Landbank of the Philippines
-                    </span>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="3" style="border-left:1px solid white;border-right:1px solid white;">
-                    <span>
-                        Location: <?php
-                                    if (!empty($province)) {
-                                        // echo Yii::$app->memem->cibrCdrHeader($province)['location'];
-                                        echo $prov['location'];
-                                    }
-                                    ?>
-                    </span>
-                </th>
+                                                            if (!empty($province)) {
+                                                                // echo $prov[$province]['province'];
+                                                                // echo Yii::$app->memem->cibrCdrHeader($province)['province'];
+                                                                echo $prov['province'];
+                                                            }
+                                                            ?></span>
+                    </th>
+                    <th colspan="3" class="header">
+                        <span>
+                            Station: Surigao del Norte
+                        </span>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="9" rowspan="2" style="border-left:1px solid white;border-right:1px solid white;">
+                        <span> Fund Cluster : <?php
+                                                if (!empty($book)) {
 
-            </tr>
-            <tr>
+                                                    echo $book;
+                                                }
+                                                ?></span>
+                    </th>
+                    <th colspan="3" class="header" style="border-left:1px solid white;border-right:1px solid white;">
+                        <span>
+                            Bank : Landbank of the Philippines
+                        </span>
+                    </th>
+                </tr>
+                <tr>
+                    <th colspan="3" style="border-left:1px solid white;border-right:1px solid white;">
+                        <span>
+                            Location: <?php
+                                        if (!empty($province)) {
+                                            // echo Yii::$app->memem->cibrCdrHeader($province)['location'];
+                                            echo $prov['location'];
+                                        }
+                                        ?>
+                        </span>
+                    </th>
 
-                <th class='head' rowspan="6">Date</th>
-                <th class='head' rowspan="6">Check No.</th>
-                <th class='head' rowspan="6">Particular</th>
-                <th class='head' rowspan="3" colspan="3">CASH IN BANK</th>
-            </tr>
+                </tr>
+                <tr>
 
-            <tr>
-                <th colspan="6" style="text-align: center;">BREAKDOWN </th>
-            </tr>
-            <tr>
-                <th class='head' colspan="2">PERSONNEL SERVICES </th>
-                <th class='head' colspan="1">MAINTENANCE AND OTHER OPERATING EXPENSES </th>
-                <th class='head' colspan="3">OTHERS</th>
-            </tr>
+                    <th class='head' rowspan="6">Date</th>
+                    <th class='head' rowspan="6">Check No.</th>
+                    <th class='head' rowspan="6">Particular</th>
+                    <th class='head' rowspan="3" colspan="3">CASH IN BANK</th>
+                </tr>
+
+                <tr>
+                    <th colspan="6" style="text-align: center;">BREAKDOWN </th>
+                </tr>
+                <tr>
+                    <th class='head' colspan="2">PERSONNEL SERVICES </th>
+                    <th class='head' colspan="1">MAINTENANCE AND OTHER OPERATING EXPENSES </th>
+                    <th class='head' colspan="3">OTHERS</th>
+                </tr>
 
 
-            <tr>
-                <th class='head' rowspan="3">Deposits</th>
-                <th class='head' rowspan="3">Withdrawals</th>
-                <th class='head' rowspan="3"> Balances</th>
-            </tr>
-            <tr>
-                <th rowspan="1">Salaries and Wages-Casual</th>
-                <th rowspan="1">Salaries and Wages -Casual/ Contractual</th>
-                <!-- <th rowspan="1"> 3</th> -->
-                <th rowspan="1"> Office Supplies Expenses </th>
-                <!-- <th rowspan="1"> 5</th>
+                <tr>
+                    <th class='head' rowspan="3">Deposits</th>
+                    <th class='head' rowspan="3">Withdrawals</th>
+                    <th class='head' rowspan="3"> Balances</th>
+                </tr>
+                <tr>
+                    <th rowspan="1">Salaries and Wages-Casual</th>
+                    <th rowspan="1">Salaries and Wages -Casual/ Contractual</th>
+                    <!-- <th rowspan="1"> 3</th> -->
+                    <th rowspan="1"> Office Supplies Expenses </th>
+                    <!-- <th rowspan="1"> 5</th>
                 <th rowspan="1"> 6</th> -->
-                <th class='head' rowspan="2"> Account Description</th>
-                <th class='head' rowspan="2">UACS Code</th>
-                <th class='head' rowspan="2">Amount</th>
-            </tr>
-            <tr>
-                <th rowspan="1">50101020</th>
-                <th rowspan="1">50101020</th>
-                <!-- <th rowspan="1"> 3</th> -->
-                <th rowspan="1"> 50201010</th>
-                <!-- <th rowspan="1"> 5</th>
+                    <th class='head' rowspan="2"> Account Description</th>
+                    <th class='head' rowspan="2">UACS Code</th>
+                    <th class='head' rowspan="2">Amount</th>
+                </tr>
+                <tr>
+                    <th rowspan="1">50101020</th>
+                    <th rowspan="1">50101020</th>
+                    <!-- <th rowspan="1"> 3</th> -->
+                    <th rowspan="1"> 50201010</th>
+                    <!-- <th rowspan="1"> 5</th>
                 <th rowspan="1"> 6</th> -->
-            </tr>
-        </thead>
-        <tbody>
+                </tr>
+            </thead>
+            <tbody>
 
-            <?php
-            $total_deposit = 0;
-            $total_withdrawals = 0;
-            $balance = !empty($beginning_balance) ? $beginning_balance : 0;
-            $x = 0;
-            if (!empty($dataProvider)) {
-                foreach ($dataProvider as $i => $data) {
-                    $balance += (float)$data['amount'] - (float)$data['withdrawals'];
-                    // if ($data['reporting_period'] === $reporting_period) {
-                    if ($x === 0) {
-                        echo "<tr>
+                <?php
+                $total_deposit = 0;
+                $total_withdrawals = 0;
+                $balance = !empty($beginning_balance) ? $beginning_balance : 0;
+                $x = 0;
+                if (!empty($dataProvider)) {
+                    foreach ($dataProvider as $i => $data) {
+                        $balance += (float)$data['amount'] - (float)$data['withdrawals'];
+                        // if ($data['reporting_period'] === $reporting_period) {
+                        if ($x === 0) {
+                            echo "<tr>
                   
                             <td></td>
                             <td></td>
@@ -204,9 +205,9 @@ $this->params['breadcrumbs'][] = $this->title;
                             <td></td>
                             <td ></td>
                          </tr>";
-                        $x++;
-                    }
-                    echo "<tr>
+                            $x++;
+                        }
+                        echo "<tr>
                   
                             <td>" . $data['check_date'] . "</td>
                             <td>" . $data['check_number'] . "</td>
@@ -221,12 +222,12 @@ $this->params['breadcrumbs'][] = $this->title;
                             <td>" . $data['gl_object_code'] . "</td>
                             <td style='text-align:right'>" . number_format((float)$data['withdrawals'], 2)  . "</td>
                          </tr>";
-                    $total_deposit += floatval((float)$data['amount']);
-                    $total_withdrawals += floatval((float)$data['withdrawals']);
-                    // }
-                }
+                        $total_deposit += floatval((float)$data['amount']);
+                        $total_withdrawals += floatval((float)$data['withdrawals']);
+                        // }
+                    }
 
-                echo "<tr>
+                    echo "<tr>
                 <td></td>
                 <td colspan='2' style='text-align:center;font-weight:bold'>Total</td>
                 <td style='text-align:right;font-weight:bold'>" . number_format($total_deposit, 2) . "</td>
@@ -240,38 +241,50 @@ $this->params['breadcrumbs'][] = $this->title;
              
                 <td style='text-align:right;font-weight:bold'>" . number_format($total_withdrawals, 2) . "</td>
                 </tr>";
-                //     echo "<pre>";
-                //         var_dump($dataProvider);
-                //    echo" </pre>";
-            }
+                    //     echo "<pre>";
+                    //         var_dump($dataProvider);
+                    //    echo" </pre>";
+                }
 
 
-            ?>
+                ?>
 
-            <tr>
-                <td colspan="8" style="border-right:none;"> </td>
-                <td colspan="2" style="text-align: center;border-left:none;border-right:none">
+                <tr>
+                    <td colspan="8" style="border-right:none;"> </td>
+                    <td colspan="2" style="text-align: center;border-left:none;border-right:none">
 
-                    <div class="foot" style="text-align: left;margin-top:2rem;">Certified Correct:</div>
-                    <div class="foot" style="font-weight: bold; margin-top:2rem;text-transform:uppercase"><?php
+                        <div class="foot" style="text-align: left;margin-top:2rem;">Certified Correct:</div>
+                        <div class="foot" style="font-weight: bold; margin-top:2rem;text-transform:uppercase"><?php
 
-                                                                                                            if (!empty($province)) {
-                                                                                                                echo $prov['officer'];
-                                                                                                            }
-                                                                                                            ?></div>
-                    <div class="foot">Signature</div>
-                    <div class="foot">Disbursing Officer</div>
+                                                                                                                if (!empty($province)) {
+                                                                                                                    echo $prov['officer'];
+                                                                                                                }
+                                                                                                                ?></div>
+                        <div class="foot">Signature</div>
+                        <div class="foot">Disbursing Officer</div>
 
-                </td>
-                <td colspan="2" style="border-left: none;"></td>
-            </tr>
+                    </td>
+                    <td colspan="2" style="border-left: none;"></td>
+                </tr>
 
-        </tbody>
+            </tbody>
 
-    </table>
-    <?php Pjax::end() ?>
+        </table>
+        <?php Pjax::end() ?>
+    </div>
+
+    <div id="dots5">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
 </div>
 <style>
+    #con {
+        display: none;
+    }
+
     table,
     th,
     td {
@@ -311,6 +324,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php
 $this->registerJsFile(yii::$app->request->baseUrl . "/js/select2.min.js", ['depends' => [\yii\web\JqueryAsset::class]]);
 $this->registerJsFile(yii::$app->request->baseUrl . "/js/jquery.dataTables.js", ['depends' => [\yii\web\JqueryAsset::class]]);
+$this->registerCssFile(yii::$app->request->baseUrl . "/frontend/web/css/site.css", ['depends' => [\yii\web\JqueryAsset::class]]);
 ?>
 <?php
 SweetAlertAsset::register($this);
@@ -324,6 +338,13 @@ $script = <<< JS
             })
         })
 
+        $(document).ready(function(){
+      
+            setTimeout(() => {
+                $("#con").show()
+            $("#dot5").hide()
+            }, 1000);
+        })
 JS;
 $this->registerJs($script);
 ?>
