9832630319cc2b0b4c78761e3b955fb4dabe8594
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-08-13 14:49:34 +0800

    Update ChartOfAccountsController.php

--- frontend/controllers/ChartOfAccountsController.php
+++ frontend/controllers/ChartOfAccountsController.php
@@ -464,7 +464,7 @@ class ChartOfAccountsController extends Controller
 
 
         $query2 = (new \yii\db\Query())
-            ->select('*')
+            ->select('object_code,account_title')
             ->from('accounting_codes')
             ->where('is_active =1 AND coa_is_active = 1 AND sub_account_is_active = 1');
         if (!empty($query1)) {
