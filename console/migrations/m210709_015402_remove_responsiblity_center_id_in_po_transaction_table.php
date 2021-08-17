7df615a475bd0eab2526d6215519ae3dfbe584db
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-08-17 18:42:31 +0800

    Update m210709_015402_remove_responsiblity_center_id_in_po_transaction_table.php

--- console/migrations/m210709_015402_remove_responsiblity_center_id_in_po_transaction_table.php
+++ console/migrations/m210709_015402_remove_responsiblity_center_id_in_po_transaction_table.php
@@ -13,17 +13,17 @@ class m210709_015402_remove_responsiblity_center_id_in_po_transaction_table exte
     public function safeUp()
     {
         // drops foreign key for table `{{%advances}}`
-        $this->dropForeignKey(
-            '{{%fk-po_transaction-responsibility_center_id}}',
-            '{{%po_transaction}}'
-        );
+        // $this->dropForeignKey(
+        //     '{{%fk-po_transaction-responsibility_center_id}}',
+        //     '{{%po_transaction}}'
+        // );
 
-        // drops index for column `responsibility_center_id`
-        $this->dropIndex(
-            '{{%idx-po_transaction-responsibility_center_id}}',
-            '{{%po_transaction}}'
-        );
-        $this->dropColumn('po_transaction', 'responsibility_center_id');
+        // // drops index for column `responsibility_center_id`
+        // $this->dropIndex(
+        //     '{{%idx-po_transaction-responsibility_center_id}}',
+        //     '{{%po_transaction}}'
+        // );
+        // $this->dropColumn('po_transaction', 'responsibility_center_id');
     }
 
     /**
@@ -31,22 +31,22 @@ class m210709_015402_remove_responsiblity_center_id_in_po_transaction_table exte
      */
     public function safeDown()
     {
-        $this->addColumn('po_transaction', 'responsibility_center_id', $this->integer());
-        $this->createIndex(
-            '{{%idx-po_transaction-responsibility_center_id}}',
-            '{{%po_transaction}}',
-            'responsibility_center_id'
-        );
+        // $this->addColumn('po_transaction', 'responsibility_center_id', $this->integer());
+        // $this->createIndex(
+        //     '{{%idx-po_transaction-responsibility_center_id}}',
+        //     '{{%po_transaction}}',
+        //     'responsibility_center_id'
+        // );
 
-        // add foreign key for table `{{%transaction}}`
-        $this->addForeignKey(
-            '{{%fk-po_transaction-responsibility_center_id}}',
-            '{{%po_transaction}}',
-            'responsibility_center_id',
-            '{{%responsibility_center}}',
-            'id',
-            'CASCADE'
-        );
+        // // add foreign key for table `{{%transaction}}`
+        // $this->addForeignKey(
+        //     '{{%fk-po_transaction-responsibility_center_id}}',
+        //     '{{%po_transaction}}',
+        //     'responsibility_center_id',
+        //     '{{%responsibility_center}}',
+        //     'id',
+        //     'CASCADE'
+        // );
     }
 
     /*
