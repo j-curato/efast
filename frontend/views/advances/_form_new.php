75d65629a7ad9be33309f4ddc79c23cefd57e8e2
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-08-26 14:49:52 +0800

    Update _form_new.php

--- frontend/views/advances/_form_new.php
+++ frontend/views/advances/_form_new.php
@@ -379,21 +379,6 @@ use yii\helpers\ArrayHelper;
 
 
     var fund_source_type = []
-    $(document).ready(function() {
-
-        $.getJSON('/afms/frontend/web/index.php?r=database-view/sub-accounts')
-            .then(function(data) {
-                var array = []
-                $.each(data, function(key, val) {
-                    array.push({
-                        id: val.object_code,
-                        text: val.object_code + ' ' + val.account_title
-                    })
-                })
-                accounts = array
-            })
-
-    })
 </script>
 <?php
 
@@ -451,6 +436,44 @@ $script = <<<JS
             }
         })
     })
+   function subAccounts(){
+        return $.getJSON('afms/frontend/web/index.php?r=database-view/sub-accounts')
+    }
+    $.when(subAccounts()).done(function(sub_accounts){
+                var array = []
+                $.each(sub_accounts, function(key, val) {
+                    array.push({
+                        id: val.object_code,
+                        text: val.object_code + ' ' + val.account_title
+                    })
+                })
+                accounts = array
+                if ($('#update_id').val()>0){
+
+                    $.ajax({
+                        type:'POST',
+                        url:window.location.pathname + "?r=advances/update-advances",
+                        data:{
+                            update_id:$('#update_id').val()
+                        },
+                        success:function(data){
+                            var res= JSON.parse(data)
+                            console.log(res)
+
+                            $('#report').val(res[0]['report_type']).trigger('change');
+                            $('#province').val(res[0]['province']).trigger('change');
+                            $('#particular').val(res[0]['particular']).trigger('change');
+                            $('#reporting_period').val(res[0]['reporting_period'])
+                            addToTransactionTable(res)
+
+                        }
+                        
+
+                    })
+
+                }   
+
+    })
     $(document).ready(function(){
         getFundSourceType().then((data) => {
             var array = []
@@ -473,30 +496,7 @@ $script = <<<JS
 
         // KUNG NAAY SULOD ANG UPDATE ID MAG POPULATE SA MGA DATA
 
-        if ($('#update_id').val()>0){
-
-            $.ajax({
-                type:'POST',
-                url:window.location.pathname + "?r=advances/update-advances",
-                data:{
-                    update_id:$('#update_id').val()
-                },
-                success:function(data){
-                    var res= JSON.parse(data)
-                    console.log(res)
-
-                    $('#report').val(res[0]['report_type']).trigger('change');
-                    $('#province').val(res[0]['province']).trigger('change');
-                    $('#particular').val(res[0]['particular']).trigger('change');
-                    $('#reporting_period').val(res[0]['reporting_period'])
-                    addToTransactionTable(res)
-
-                }
-                
-
-            })
-
-        }   
+   
     })
 
     
