adfabdbbef674fb14f81134fc250e5283a1b1984
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2021-09-08 11:12:10 +0800

    Create m210908_031203_add_qwerqwe_on_q_table.php

---
+++ console/migrations/m210908_031203_add_qwerqwe_on_q_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210908_031203_add_qwerqwe_on_q_table
+ */
+class m210908_031203_add_qwerqwe_on_q_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m210908_031203_add_qwerqwe_on_q_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210908_031203_add_qwerqwe_on_q_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
