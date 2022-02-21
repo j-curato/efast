baaacd88470bb692e6726688107344b1cf6183b4
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-02-21 16:39:12 +0800

    q

--- frontend/views/assignatory/create.php
+++ frontend/views/assignatory/create.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Assignatory */
 
-$this->title = 'Create Assignatory';
-$this->params['breadcrumbs'][] = ['label' => 'Assignatories', 'url' => ['index']];
+$this->title = 'Create Asignatory';
+$this->params['breadcrumbs'][] = ['label' => 'Asignatories', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="assignatory-create">
