2aa83f8f59aae18f762a29e91dd05f2f29d617b5
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-04-07 14:09:20 +0800

    cadadr_balance,detailed financial positions

---
+++ console/migrations/m220407_060015_update_cadadr_balances_view.php
@@ -0,0 +1,94 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220407_060015_update_cadadr_balances_view
+ */
+class m220407_060015_update_cadadr_balances_view extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $sql = <<<SQL
+            DROP VIEW IF EXISTS cadadr_balances;
+            SELECT
+                cadadr.book_name,
+                reporting_period,
+                SUM(nca_recieve) as total_nca_recieve,
+                SUM(check_issued) as total_check_issued,
+                SUM(ada_issued) as total_ada_issued
+                FROM cadadr
+                WHERE cadadr.is_cancelled !=1
+                GROUP BY
+                cadadr.book_name,
+                reporting_period 
+
+                UNION ALL 
+
+                SELECT
+                q.book_name,
+                q.reporting_period,
+                SUM(q.nca_recieve) as total_nca_recieve,
+                SUM(q.check_issued) as total_check_issued,
+                SUM(q.ada_issued) as total_ada_issued
+                FROM 
+                (
+                SELECT
+                
+                cadadr.reporting_period,
+                cadadr.book_name,
+                0 as nca_recieve,
+                (CASE
+                WHEN QUARTER(CONCAT(cadadr.reporting_period,'-01')) = QUARTER(CONCAT(cadadr.cancelled_r_period,'-01')) AND SUBSTRING_INDEX(cadadr.reporting_period,'-',1) = SUBSTRING_INDEX(cadadr.cancelled_r_period,'-',1)
+                THEN cadadr.check_issued 
+                ELSE
+                cadadr.check_issued * (-1)
+                END
+                ) as check_issued,
+                (CASE
+                WHEN QUARTER(CONCAT(cadadr.reporting_period,'-01')) = QUARTER(CONCAT(cadadr.cancelled_r_period,'-01')) AND SUBSTRING_INDEX(cadadr.reporting_period,'-',1) = SUBSTRING_INDEX(cadadr.cancelled_r_period,'-',1)
+                THEN cadadr.ada_issued 
+                ELSE
+                cadadr.ada_issued * (-1)
+                END
+                ) as ada_issued
+                FROM cadadr
+                WHERE 
+                is_cancelled =1
+                ORDER BY issuance_date
+                )as q
+                GROUP BY
+                q.book_name,
+                q.reporting_period 
+        SQL;
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220407_060015_update_cadadr_balances_view cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220407_060015_update_cadadr_balances_view cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
--- frontend/views/report/detailed_financial_position.php
+++ frontend/views/report/detailed_financial_position.php
@@ -88,7 +88,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <table class="table" id='data-table' style="margin-top:30px">
                 <thead>
                     <tr class="main_header">
-                        <th colspan="5">
+                        <th colspan="7">
                             <div style="display:flex;width:100%;align-items:center;" class="main_head">
                                 <div style="padding:12px;">
 
@@ -111,7 +111,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         </th>
                     </tr>
                     <tr>
-                        <th colspan="3"></th>
+                        <th colspan="5"></th>
                         <th><span id="current_year"></span></th>
                         <th><span id="last_year"></span></th>
 
@@ -125,173 +125,177 @@ $this->params['breadcrumbs'][] = $this->title;
             </table>
 
         </div>
-        <style>
-            .right-border {
-                border-right: 1px solid transparent;
+
+
+    </div>
+    <style>
+        .right-border {
+            border-right: 1px solid transparent;
+            font-weight: bold;
+        }
+
+        .main_head>div>h5 {
+            font-weight: bold;
+        }
+
+        #reporting_period {
+            background-color: white;
+            border-radius: 3px;
+        }
+
+        .head {
+            text-align: left;
+        }
+
+        .table>thead>tr>th {
+            border-bottom: 1px solid black;
+            border-top: 1px solid black;
+        }
+
+
+
+        .table {
+            position: relative;
+            margin-top: 20px;
+        }
+
+        .head>th {
+            border-top: 1px solid black
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            padding: 12px;
+            background-color: white;
+            text-align: center;
+        }
+
+
+
+        table {
+            border: 1px solid black;
+            width: 100%;
+        }
+
+        .container {
+            margin-top: 5px;
+            position: relative;
+            padding: 10px;
+            /* overflow: scroll; */
+
+        }
+
+        .head>th {
+            display: table-cell;
+            vertical-align: text-top;
+            text-align: center;
+        }
+
+        thead>tr>td {
+            border: 1px solid black;
+            padding: 10px;
+            font-weight: bold;
+        }
+
+        #fund {
+            display: none;
+        }
+
+        .actions {
+            padding: 20px;
+            position: relative;
+        }
+
+
+        .document_header1>th {
+
+            padding: 10px;
+        }
+
+        .document_header1>th {
+            border: 0;
+        }
+
+        .amount {
+            text-align: right;
+        }
+
+        @media print {
+            .actions {
+                display: none;
+            }
+
+            table,
+            th,
+            td {
+                padding: 5px;
+                font-size: 10px;
+            }
+
+            thead>tr>td {
+                border: 1px solid black;
+                padding: 5px;
                 font-weight: bold;
             }
 
-            .main_head>div>h5 {
-                font-weight: bold;
+            @page {
+                size: auto;
+                margin: 0cm;
+                margin-top: 0.5cm;
             }
 
-            #reporting_period {
-                background-color: white;
-                border-radius: 3px;
-            }
-
-            .head {
-                text-align: left;
-            }
-
-            .table>thead>tr>th {
-                border-bottom: 1px solid black;
-                border-top: 1px solid black;
+            #print {
+                display: none;
             }
 
 
+            .container {
+                margin: 0;
+                top: 0;
 
-            .table {
-                position: relative;
-                margin-top: 20px;
             }
 
-            .head>th {
-                border-top: 1px solid black
+            .entity_name {
+                font-size: 5pt;
             }
 
             table,
             th,
             td {
                 border: 1px solid black;
-                padding: 12px;
+                padding: 5px;
                 background-color: white;
-                text-align: center;
-            }
-
-
-
-            table {
-                border: 1px solid black;
-                width: 100%;
             }
 
             .container {
-                margin-top: 5px;
-                position: relative;
-                padding: 10px;
-                /* overflow: scroll; */
 
+                border: none;
             }
 
-            .head>th {
-                display: table-cell;
-                vertical-align: text-top;
-                text-align: center;
+
+            table {
+                page-break-after: auto
             }
 
-            thead>tr>td {
-                border: 1px solid black;
-                padding: 10px;
-                font-weight: bold;
+            tr {
+                page-break-inside: avoid;
+                page-break-after: auto
             }
 
-            #fund {
+            td {
+                page-break-inside: avoid;
+                page-break-after: auto
+            }
+
+
+
+            .main-footer {
                 display: none;
             }
-
-            .actions {
-                padding: 20px;
-                position: relative;
-            }
-
-
-            .document_header1>th {
-
-                padding: 10px;
-            }
-
-            .document_header1>th {
-                border: 0;
-            }
-
-            @media print {
-                .actions {
-                    display: none;
-                }
-
-                table,
-                th,
-                td {
-                    padding: 5px;
-                    font-size: 10px;
-                }
-
-                thead>tr>td {
-                    border: 1px solid black;
-                    padding: 5px;
-                    font-weight: bold;
-                }
-
-                @page {
-                    size: auto;
-                    margin: 0cm;
-                    margin-top: 0.5cm;
-                }
-
-                #print {
-                    display: none;
-                }
-
-
-                .container {
-                    margin: 0;
-                    top: 0;
-
-                }
-
-                .entity_name {
-                    font-size: 5pt;
-                }
-
-                table,
-                th,
-                td {
-                    border: 1px solid black;
-                    padding: 5px;
-                    background-color: white;
-                }
-
-                .container {
-
-                    border: none;
-                }
-
-
-                table {
-                    page-break-after: auto
-                }
-
-                tr {
-                    page-break-inside: avoid;
-                    page-break-after: auto
-                }
-
-                td {
-                    page-break-inside: avoid;
-                    page-break-after: auto
-                }
-
-
-
-                .main-footer {
-                    display: none;
-                }
-            }
-        </style>
-
-    </div>
-
+        }
+    </style>
     <?php
     $this->registerJsFile(yii::$app->request->baseUrl . "/frontend/web/js/thousands_separator.js", ['depends' => [\yii\web\JqueryAsset::class]]);
     ?>
@@ -318,40 +322,113 @@ $this->params['breadcrumbs'][] = $this->title;
         })
 
         function displaData(data) {
-            console.log(data)
             $('#data-table tbody').html('')
             $.each(data, function(key, val) {
+                const account_group = key;
                 const account_group_row = `<tr>
-                <td>${key}</td>
+                <th>${key}</th>
+                <td></td>
+                <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
                 <td></td>
                 </tr>`
                 $('#data-table tbody').append(account_group_row)
-                $.each(val, function(key, val2) {
-                    const major_row = `<tr>
+                $.each(val, function(key2, val2) {
+                    const current_noncurrent = key2
+                    let total_current_current_non_current = 0;
+                    let total_prev_current_non_current = 0;
+                    if (account_group != current_noncurrent) {
+                        const current_noncurrent = `<tr>
                                             <td></td>
-                                            <td>${key}</td>
+                                            <th>${key2}</th>
+                                            <td></td>
+                                            <td></td>
                                             <td></td>
                                             <td></td>
                                             <td></td>
                                         </tr>`
-                    $('#data-table tbody').append(major_row)
-                    $.each(val2, function(key, val3) {
-                        const current_balance = val3.current_balance
-                        const prev_balance = val3.last_year_balance
-                        console.log(val3.last_year_balance)
-                        const data_row = `<tr>
+                        $('#data-table tbody').append(current_noncurrent)
+                    }
+
+                    $.each(val2, function(key3, val3) {
+                        const major_name = key3
+                        let total_current_major = 0
+                        let total_prev_major = 0
+
+                        let major = major_name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase()
+
+                        if (major_name != current_noncurrent) {
+
+                            const major_group = `<tr>
                                             <td></td>
                                             <td></td>
-                                            <td style='text-align:left;'>${val3.general_ledger}</td>
-                                            <td style='text-align:right;'>${thousands_separators(current_balance)}</td>
-                                            <td style='text-align:right;'>${thousands_separators(prev_balance)}</td>
-                                            `
-                        $('#data-table tbody').append(data_row)
+                                            <th>${key3}</th>
+                                            <td></td>
+                                            <td></td>
+                                            <th id='total_current_${major}' class='amount' ></th>
+                                            <th id='total_prev_${major}' class='amount' ></th>
+                                        </tr>`
+                            $('#data-table tbody').append(major_group)
+                        }
+
+                        $.each(val3, function(key4, val4) {
+                            const sub_major_name = key4
+                            let total_current_sub_major = 0
+                            let total_prev_sub_major = 0
+
+                            let str = sub_major_name.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
+                            if (sub_major_name != major_name) {
+                                const sub_major_group = `<tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <th>${key4}</th>
+                                            <td></td>
+                                            <td id='total_current_${str}' class='amount'></td>
+                                            <td id='total_prev_${str}' class='amount'></td>
+                                        </tr>`
+                                $('#data-table tbody').append(sub_major_group)
+                            }
+
+                            $.each(val4, function(key5, val5) {
+                                const current_balance = val5.current_balance
+                                const prev_balance = val5.last_year_balance
+                                const data_row = `<tr>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td style='text-align:left;'>${val5.general_ledger}</td>
+                                                    <td class='amount'>${thousands_separators(current_balance)}</td>
+                                                    <td class='amount'>${thousands_separators(prev_balance)}</td>
+                                                    `
+                                $('#data-table tbody').append(data_row)
+                                total_current_sub_major += parseFloat(current_balance)
+                                total_prev_sub_major += parseFloat(prev_balance)
+                                total_current_major += parseFloat(current_balance)
+                                total_prev_major += parseFloat(prev_balance)
+                                total_current_current_non_current += parseFloat(current_balance)
+                                total_prev_current_non_current += parseFloat(prev_balance)
+                            })
+
+                            $(`#total_current_${str}`).text(thousands_separators(total_current_sub_major.toFixed(2)))
+                            $(`#total_prev_${str}`).text(thousands_separators(total_prev_sub_major.toFixed(2)))
+                            $(`#total_current_${major}`).text(thousands_separators(total_current_major.toFixed(2)))
+                            $(`#total_prev_${major}`).text(thousands_separators(total_prev_major.toFixed(2)))
+                        })
+
                     })
+                    const margin_row = `<tr>
+                                                <th colspan='5'style='padding:1rem;font-size:1.5rem'> Total ${current_noncurrent}</th>
+                                                <th class='amount' style='font-size:1.5rem'>${thousands_separators(total_current_current_non_current.toFixed(2))}</th>
+                                                <th class='amount' style='font-size:1.5rem'>${thousands_separators(total_prev_current_non_current.toFixed(2))}</th>
+                                               </tr>
+                                                `
+                    $('#data-table tbody').append(margin_row)
                 })
+
             })
         }
     </script>
