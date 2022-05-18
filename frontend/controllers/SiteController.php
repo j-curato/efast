b21ed1a3d82732a1a25e3176b78176072226af78
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-05-18 10:30:00 +0800

    Update SiteController.php

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -183,24 +183,24 @@ class SiteController extends Controller
      *
      * @return mixed
      */
-    public function actionSignup()
-    {
-        $this->layout = 'register';
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($model->signup()) {
-            }
-            Yii::$app->session->setFlash(
-                'success',
-                'Thank you for registration. Please check your inbox for verification email.'
-            );
-            return $this->goHome();
-        }
+    // public function actionSignup()
+    // {
+    //     $this->layout = 'register';
+    //     $model = new SignupForm();
+    //     if ($model->load(Yii::$app->request->post())) {
+    //         if ($model->signup()) {
+    //         }
+    //         Yii::$app->session->setFlash(
+    //             'success',
+    //             'Thank you for registration. Please check your inbox for verification email.'
+    //         );
+    //         return $this->goHome();
+    //     }
 
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
+    //     return $this->render('signup', [
+    //         'model' => $model,
+    //     ]);
+    // }
 
     /**
      * Requests password reset.
