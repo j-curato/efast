29405b866b7967eeb7c939ca0fbc7e167acf006f
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-06-13 13:24:57 +0800

    Update MyComponent.php

--- frontend/components/MyComponent.php
+++ frontend/components/MyComponent.php
@@ -317,102 +317,47 @@ class MyComponent extends Component
     }
     function convertNumberToWord($num = false)
     {
-        $decones = array(
-            '01' => "One",
-            '02' => "Two",
-            '03' => "Three",
-            '04' => "Four",
-            '05' => "Five",
-            '06' => "Six",
-            '07' => "Seven",
-            '08' => "Eight",
-            '09' => "Nine",
-            10 => "Ten",
-            11 => "Eleven",
-            12 => "Twelve",
-            13 => "Thirteen",
-            14 => "Fourteen",
-            15 => "Fifteen",
-            16 => "Sixteen",
-            17 => "Seventeen",
-            18 => "Eighteen",
-            19 => "Nineteen"
+        $num = str_replace(array(',', ' '), '', trim($num));
+        if (!$num) {
+            return false;
+        }
+        $num = (int) $num;
+        $words = array();
+        $list1 = array(
+            '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+            'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
         );
-        $ones = array(
-            0 => " ",
-            1 => "One",
-            2 => "Two",
-            3 => "Three",
-            4 => "Four",
-            5 => "Five",
-            6 => "Six",
-            7 => "Seven",
-            8 => "Eight",
-            9 => "Nine",
-            10 => "Ten",
-            11 => "Eleven",
-            12 => "Twelve",
-            13 => "Thirteen",
-            14 => "Fourteen",
-            15 => "Fifteen",
-            16 => "Sixteen",
-            17 => "Seventeen",
-            18 => "Eighteen",
-            19 => "Nineteen"
+        $list2 = array('', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred');
+        $list3 = array(
+            '', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion',
+            'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quattuordecillion',
+            'quindecillion', 'sexdecillion', 'septendecillion', 'octodecillion', 'novemdecillion', 'vigintillion'
         );
-        $tens = array(
-            0 => "",
-            2 => "Twenty",
-            3 => "Thirty",
-            4 => "Forty",
-            5 => "Fifty",
-            6 => "Sixty",
-            7 => "Seventy",
-            8 => "Eighty",
-            9 => "Ninety"
-        );
-        $hundreds = array(
-            "Hundred",
-            "Thousand",
-            "Million",
-            "Billion",
-            "Trillion",
-            "Quadrillion"
-        ); //limit t quadrillion 
-        $num = number_format($num, 2, ".", ",");
-        $num_arr = explode(".", $num);
-        $wholenum = $num_arr[0];
-        $decnum = $num_arr[1];
-        $whole_arr = array_reverse(explode(",", $wholenum));
-        krsort($whole_arr);
-        $rettxt = "";
-        foreach ($whole_arr as $key => $i) {
-            if ($i < 20) {
-                $rettxt .= $ones[$i];
-            } elseif ($i < 100) {
-                $rettxt .= $tens[substr($i, 0, 1)];
-                $rettxt .= " " . $ones[substr($i, 1, 1)];
+        $num_length = strlen($num);
+        $levels = (int) (($num_length + 2) / 3);
+        $max_length = $levels * 3;
+        $num = substr('00' . $num, -$max_length);
+        $num_levels = str_split($num, 3);
+        for ($i = 0; $i < count($num_levels); $i++) {
+            $levels--;
+            $hundreds = (int) ($num_levels[$i] / 100);
+            $hundreds = ($hundreds ? ' ' . $list1[$hundreds] . ' hundred' . ' ' : '');
+            $tens = (int) ($num_levels[$i] % 100);
+            $singles = '';
+            if ($tens < 20) {
+                $tens = ($tens ? ' ' . $list1[$tens] . ' ' : '');
             } else {
-                $rettxt .= $ones[substr($i, 0, 1)] . " " . $hundreds[0];
-                $rettxt .= " " . $tens[substr($i, 1, 1)];
-                $rettxt .= " " . $ones[substr($i, 2, 1)];
+                $tens = (int)($tens / 10);
+                $tens = ' ' . $list2[$tens] . ' ';
+                $singles = (int) ($num_levels[$i] % 10);
+                $singles = ' ' . $list1[$singles] . ' ';
             }
-            if ($key > 0) {
-                $rettxt .= " " . $hundreds[$key] . " ";
-            }
+            $words[] = $hundreds . $tens . $singles . (($levels && (int) ($num_levels[$i])) ? ' ' . $list3[$levels] . ' ' : '');
+        } //end for loop
+        $commas = count($words);
+        if ($commas > 1) {
+            $commas = $commas - 1;
         }
-        $rettxt = $rettxt . " peso/s";
-
-        if ($decnum > 0) {
-            $rettxt .= " and ";
-            if ($decnum < 20) {
-                $rettxt .= $decones[$decnum];
-            } elseif ($decnum < 100) {
-                $rettxt .= $tens[substr($decnum, 0, 1)];
-                $rettxt .= " " . $ones[substr($decnum, 1, 1)];
-            }
-            $rettxt = $rettxt . " centavo/s";
-        }
-        return $rettxt;
+        return implode(' ', $words);
     }
 }
