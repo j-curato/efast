0710d5a5049244550e78891d86c63e52485b005c
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-06-16 08:31:11 +0800

    Update index.php

--- frontend/views/ro-liquidation-report/index.php
+++ frontend/views/ro-liquidation-report/index.php
@@ -1,13 +1,13 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\RoLiquidationReportSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Ro Liquidation Reports';
+$this->title = 'Liquidation Reports';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="ro-liquidation-report-index">
@@ -15,24 +15,29 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Ro Liquidation Report', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Liquidation Report', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); 
+    ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'panel' => [
+            'type' => 'primary',
+            'heading' => ' Liquidation Report'
+        ],
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
             'liquidation_report_number',
             'date',
-            'updated_at',
-            'created_at',
 
-            ['class' => 'yii\grid\ActionColumn'],
+
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update}'
+            ],
         ],
     ]); ?>
 
