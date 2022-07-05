7a3cb3ff6a37fa766f257198418296069d0163ba
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-07-05 10:48:25 +0800

    Update index.php

--- frontend/views/property/index.php
+++ frontend/views/property/index.php
@@ -87,13 +87,13 @@ $this->params['breadcrumbs'][] = $this->title;
                     return   $specs;
                 }
             ],
-            [
-                'attribute' => 'description',
-                'value' => function ($model) {
-                    $specs = preg_replace('#\[n\]#', "\n", $model->description);
-                    return   $specs;
-                }
-            ],
+            // [
+            //     'attribute' => 'description',
+            //     'value' => function ($model) {
+            //         $specs = preg_replace('#\[n\]#', "\n", $model->description);
+            //         return   $specs;
+            //     }
+            // ],
             'iar_number',
             [
                 'label' => 'Unit of Measure',
