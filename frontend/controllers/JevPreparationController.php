46c0f304e4fe70b33e23ab153a76e6714ff56b3d
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-07-06 13:33:32 +0800

    Update JevPreparationController.php

--- frontend/controllers/JevPreparationController.php
+++ frontend/controllers/JevPreparationController.php
@@ -2075,7 +2075,7 @@ class JevPreparationController extends Controller
             AND accounting_codes.object_code !=accounting_codes.coa_object_code
             $and $sql2
             ) as q 
-            ORDER BY q.row_num ASC
+            ORDER BY q.date ASC
             ")
             ->bindValue(':from_reporting_period', $from_reporting_period)
             ->bindValue(':to_reporting_period', $to_reporting_period)
