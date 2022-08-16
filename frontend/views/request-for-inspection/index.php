65432f2436a0e6c66259510134c6bc0301af7875
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-08-16 10:00:14 +0800

    Update index.php

--- frontend/views/request-for-inspection/index.php
+++ frontend/views/request-for-inspection/index.php
@@ -9,15 +9,14 @@ use kartik\grid\GridView;
 
 $this->title = 'Request For Inspections';
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="request-for-inspection-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('Create Request For Inspection', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-
     <?php // echo $this->render('_search', ['model' => $searchModel]); 
     ?>
 
@@ -28,13 +27,67 @@ $this->params['breadcrumbs'][] = $this->title;
             'type' => 'primary',
             'heading' => 'Request For Inspection',
         ],
+        'pjax' => true,
         'columns' => [
 
             'rfi_number',
             'date',
-            'fk_chairperson',
-            'fk_inspector',
-            //'fk_property_unit',
+            [
+                'attribute' => 'fk_pr_office_id',
+                'value' => function ($model) {
+                    $emp = '';
+                    if (!empty($model->fk_pr_office_id)) {
+                        $dvsn = !empty($model->office->division) ? $model->office->division : '';
+                        $unit = !empty($model->office->unit) ? $model->office->unit : '';
+                        $emp =  $dvsn . '-' .  $unit;
+                    }
+                    return $emp;
+                }
+            ],
+            [
+                'attribute' => 'unit_head',
+                'value' => function ($model) {
+                    $emp = '';
+                    if (!empty($model->office->id)) {
+
+                        if (!empty($model->office->unitHead->f_name)) {
+                            $emp = $model->office->unitHead->f_name . ' ' .  $model->office->unitHead->m_name[0] . '. ' .  $model->office->unitHead->l_name;
+                        }
+                        // $emp = $model->office->unitHead->f_name;
+                    }
+                    return $emp;
+                }
+            ],
+            [
+                'attribute' => 'fk_chairperson',
+                'value' => function ($model) {
+                    $emp = '';
+                    if (!empty($model->fk_chairperson)) {
+                        $emp = $model->chairperson->f_name . ' ' .  $model->chairperson->m_name[0] . '. ' .  $model->chairperson->l_name;
+                    }
+                    return $emp;
+                }
+            ],
+            [
+                'attribute' => 'fk_inspector',
+                'value' => function ($model) {
+                    $emp = '';
+                    if (!empty($model->fk_inspector)) {
+                         $emp = $model->inspector->f_name . ' ' .  $model->inspector->m_name[0] . '. ' .  $model->inspector->l_name;
+                    }
+                    return $emp;
+                }
+            ],
+            [
+                'attribute' => 'fk_property_unit',
+                'value' => function ($model) {
+                    $emp = '';
+                    if (!empty($model->fk_property_unit)) {
+                        $emp = $model->propertyUnit->f_name . ' ' .  $model->propertyUnit->m_name[0] . '. ' .  $model->propertyUnit->l_name;
+                    }
+                    return $emp;
+                }
+            ],
             [
                 'label' => 'Action',
                 'format' => 'raw',
