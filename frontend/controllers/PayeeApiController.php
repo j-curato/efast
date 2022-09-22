d8f5aa72d931ca50307dea36c0ca6e4402a9f01e
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-09-22 16:50:28 +0800

    Update PayeeApiController.php

--- frontend/controllers/PayeeApiController.php
+++ frontend/controllers/PayeeApiController.php
@@ -26,6 +26,10 @@ class PayeeApiController extends \yii\rest\ActiveController
     {
         $actions = parent::actions();
         unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        unset($actions['index']);
+        unset($actions['view']);
     }
     public function actionCreate()
     {
@@ -33,12 +37,12 @@ class PayeeApiController extends \yii\rest\ActiveController
         $transaction = Yii::$app->db->beginTransaction();
         $source_jason = Yii::$app->getRequest()->getBodyParams();
         $source_payee = $source_jason;
-        $target_payee = Yii::$app->db->createCommand('SELECT * FROM payee')->queryAll();
-        $source_payee_difference = array_map(
-            'unserialize',
-            array_diff(array_map('serialize', $source_payee), array_map('serialize', $target_payee))
+        // $target_payee = Yii::$app->db->createCommand('SELECT * FROM payee')->queryAll();
+        // $source_payee_difference = array_map(
+        //     'unserialize',
+        //     array_diff(array_map('serialize', $source_payee), array_map('serialize', $target_payee))
 
-        );
+        // );
         // return json_encode($source_payee_difference);
 
         // var_dump($source_payee_difference);
@@ -54,33 +58,23 @@ class PayeeApiController extends \yii\rest\ActiveController
                             ->bindValue(':id', $val['id'])
                             ->queryScalar();
                         if (intval($query) === 1) {
-                            $update_payee = Payee::findOne($val['id']);
-                            $update_payee->id = $val['id'];
-                            $update_payee->account_name = $val['account_name'];
-                            $update_payee->registered_name = $val['registered_name'];
-                            $update_payee->contact_person = $val['contact_person'];
-                            $update_payee->registered_address = $val['registered_address'];
-                            $update_payee->contact = $val['contact'];
-                            $update_payee->remark = $val['remark'];
-                            $update_payee->tin_number = $val['tin_number'];
-                            $update_payee->isEnable = $val['isEnable'];
-                            if ($update_payee->save(false)) {
-                            } else {
-                                return json_encode('wala na save');
-                            }
+                            $payee_tbl = Payee::findOne($val['id']);
                         } else {
-                            $new_payee = new Payee();
-                            $new_payee->id = $val['id'];
-                            $new_payee->account_name = $val['account_name'];
-                            $new_payee->registered_name = $val['registered_name'];
-                            $new_payee->contact_person = $val['contact_person'];
-                            $new_payee->registered_address = $val['registered_address'];
-                            $new_payee->contact = $val['contact'];
-                            $new_payee->remark = $val['remark'];
-                            $new_payee->tin_number = $val['tin_number'];
-                            $new_payee->isEnable = $val['isEnable'];
-                            if ($new_payee->save(false)) {
-                            }
+                            $payee_tbl = new Payee();
+                        }
+                        $payee_tbl->id = $val['id'];
+                        $payee_tbl->account_name = $val['account_name'];
+                        $payee_tbl->registered_name = $val['registered_name'];
+                        $payee_tbl->contact_person = $val['contact_person'];
+                        $payee_tbl->registered_address = $val['registered_address'];
+                        $payee_tbl->contact = $val['contact'];
+                        $payee_tbl->remark = $val['remark'];
+                        $payee_tbl->tin_number = $val['tin_number'];
+                        $payee_tbl->isEnable = $val['isEnable'];
+                        if ($payee_tbl->save(false)) {
+                        } else {
+                            $transaction->rollBack();
+                            return 'failed';
                         }
                     }
                 }
