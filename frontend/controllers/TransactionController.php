043aba7c3756022bb0700355b096413f3e4e47fe
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-12-01 15:38:24 +0800

    Update TransactionController.php

--- frontend/controllers/TransactionController.php
+++ frontend/controllers/TransactionController.php
@@ -169,16 +169,16 @@ class TransactionController extends Controller
 
             $model->tracking_number = $this->getTrackingNumber($model->responsibility_center_id, 1, $model->transaction_date);
             $model->id = Yii::$app->db->createCommand("SELECT UUID_SHORT()")->queryScalar();
-            // if ($ip !== '192.168.10.116') {
-            //     if (
-            //         $division === 'idd' ||
-            //         $division === 'sdd' ||
-            //         $division === 'fad'
-            //     ) {
-            //         return;
-            //     }
-            //     $model->is_local = 0;
-            // }
+            if ($ip !== '192.168.1.21') {
+                if (
+                    $division === 'idd' ||
+                    $division === 'sdd' ||
+                    $division === 'fad'
+                ) {
+                    return;
+                }
+                $model->is_local = 0;
+            }
             if ($model->save()) {
 
                 $this->insertTransactionIar($model->id, $iars);
