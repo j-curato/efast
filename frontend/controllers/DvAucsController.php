38f64690e23b601198cbf5673322547a02c08288
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-12-01 10:06:20 +0800

    Update DvAucsController.php

--- frontend/controllers/DvAucsController.php
+++ frontend/controllers/DvAucsController.php
@@ -1576,15 +1576,15 @@ class DvAucsController extends Controller
                         }
 
 
-                        $flag =  $this->insertDvEntries(
-                            $model->id,
-                            $ors,
-                            $amount_disbursed,
-                            $vat,
-                            $ewt_goods_services,
-                            $compensation,
-                            $liabilities
-                        );
+                        // $flag =  $this->insertDvEntries(
+                        //     $model->id,
+                        //     $ors,
+                        //     $amount_disbursed,
+                        //     $vat,
+                        //     $ewt_goods_services,
+                        //     $compensation,
+                        //     $liabilities
+                        // );
                     } else {
                         $flag = false;
                     }
