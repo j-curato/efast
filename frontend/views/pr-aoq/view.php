d04cb46453d5ca75ea05d423f3518c34cd3cc208
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2022-12-06 15:51:23 +0800

    Update view.php

--- frontend/views/pr-aoq/view.php
+++ frontend/views/pr-aoq/view.php
@@ -104,7 +104,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <tr>
                     <th colspan="<?= $header_count ?>" style='padding:0;border:none;'>
                         <?php
-                        $date = DateTime::createFromFormat('Y-m-d H:i:s', $model->rfq->created_at);
+                        $date = DateTime::createFromFormat('Y-m-d', $model->rfq->deadline);
                         echo $date->format('F d, Y');
 
                         ?>
