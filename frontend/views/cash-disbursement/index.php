736cd86926dffed8ff0801a82ef8176f2e25afc1
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-01-16 13:39:34 +0800

    Update index.php

--- frontend/views/cash-disbursement/index.php
+++ frontend/views/cash-disbursement/index.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Create Cash Disbursement', ['create'], ['class' => 'btn btn-success']) ?>
-        <button class="btn btn-success" data-target="#uploadmodal" data-toggle="modal">Import</button>
+        <!-- <button class="btn btn-success" data-target="#uploadmodal" data-toggle="modal">Import</button> -->
     </p>
 
     <div class="modal fade" id="uploadmodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
