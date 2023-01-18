fb2075ea11371474c09c49543abf273552f630fb
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-01-18 11:50:51 +0800

    Create m230118_030546_add_office_and_division_id_in_purchase_request_table.php

---
+++ console/migrations/m230118_030546_add_office_and_division_id_in_purchase_request_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230118_030546_add_office_and_division_id_in_purchase_request_table
+ */
+class m230118_030546_add_office_and_division_id_in_purchase_request_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('pr_purchase_request', 'fk_office_id', $this->integer());
+        $this->addColumn('pr_purchase_request', 'fk_division_id', $this->bigInteger());
+        $this->addColumn('pr_purchase_request', 'fk_division_program_unit_id', $this->integer());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('pr_purchase_request', 'fk_office_id');
+        $this->dropColumn('pr_purchase_request', 'fk_division_id');
+        $this->dropColumn('pr_purchase_request', 'fk_division_program_unit_id');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230118_030546_add_office_and_division_id_in_purchase_request_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
