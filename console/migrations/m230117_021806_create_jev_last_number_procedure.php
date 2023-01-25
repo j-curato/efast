180768e25418cfb0d666dd60df9daaaeda157953
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-01-25 09:29:56 +0800

    Update m230117_021806_create_jev_last_number_procedure.php

--- console/migrations/m230117_021806_create_jev_last_number_procedure.php
+++ console/migrations/m230117_021806_create_jev_last_number_procedure.php
@@ -12,13 +12,11 @@ class m230117_021806_create_jev_last_number_procedure extends Migration
      */
     public function safeUp()
     {
-        $sql  = <<< SQL
+        Yii::$app->db->createCommand("
             DROP PROCEDURE IF EXISTS jev_last_number;
+            DELIMITER //
             CREATE PROCEDURE jev_last_number (IN  reporting_period VARCHAR(10),IN book_id INT,IN ref_number VARCHAR(10) )
             BEGIN
-
-
-
             SET @row := 0;
             WITH seq AS 
             (
@@ -36,18 +34,16 @@ class m230117_021806_create_jev_last_number_procedure extends Migration
             ),
             vacant_num as (SELECT  seq.`row` FROM seq LEFT JOIN used_num ON seq.`row` = used_num.p_number WHERE used_num.p_number IS NULL),
             l_num as (SELECT used_num.p_number +1 as l_num FROM used_num ORDER BY used_num.p_number DESC LIMIT 1)
-
             SELECT 
-
             IFNULL(
             (SELECT vacant_num.`row` FROM vacant_num ORDER BY vacant_num.`row` ASC LIMIT 1)
             ,l_num.l_num
             ) as  q
             FROM  l_num;
-            END 
-        SQL;
-
-        $this->execute($sql);
+            END //
+            DELIMTER ;
+            ")
+            ->execute();
     }
 
     /**
