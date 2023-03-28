9b3df40956cf6877a4d768b6c2cbdccbdef52b37
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-03-28 15:31:47 +0800

    q

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -118,7 +118,7 @@ return [
             'class' => 'frontend\components\MyComponent',
         ],
         'Myhelper' => [
-            'class' => 'frontend\components\MyHelper',
+            'class' => 'frontend\components\helpers\MyHelper',
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
