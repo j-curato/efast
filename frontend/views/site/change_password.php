4e39d7f7243f6439030f29a289c99440fd5f5d70
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-04-11 13:20:34 +0800

    Create change_password.php

---
+++ frontend/views/site/change_password.php
@@ -0,0 +1,132 @@
+<?php
+
+use aryelds\sweetalert\SweetAlertAsset;
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use yii\captcha\Captcha;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+$this->title = 'Change Password';
+
+$fieldOptions1 = [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"
+];
+
+$fieldOptions2 = [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
+];
+?>
+
+
+<div class="">
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'changePass',
+        // 'enableAjaxValidation' => true,
+        // 'enableClientValidation' => false,
+    ]); ?>
+
+    <div class="row">
+
+        <div class="col-sm-3">
+            <?= $form
+                ->field($model, 'old_password')
+                ->textInput(['placeholder' => $model->getAttributeLabel('current '),]) ?>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <?= $form
+                ->field($model, 'new_password', $fieldOptions2)
+                ->passwordInput(['placeholder' => $model->getAttributeLabel('new '), 'options' => ['autocomplete' => 'off']]) ?>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <?= $form
+                ->field($model, 'repeat_password', $fieldOptions2)
+                ->passwordInput(['placeholder' => $model->getAttributeLabel('re-type new'), 'options' => ['autocomplete' => 'off']]) ?>
+
+        </div>
+    </div>
+
+
+    <div class="row">
+
+        <div class="col-xs-1">
+            <?= Html::submitButton('Save Changes', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+        </div>
+    </div>
+
+
+    <?php ActiveForm::end(); ?>
+
+
+</div>
+
+<?php
+// SweetAlertAsset::register($this);
+// $js = <<< JS
+// $("#changePass").on("beforeSubmit", function (event) {
+//     event.preventDefault();
+//     var form = $(this);
+//     var inputIds = $('#changePass :input').map(function() {
+//         if (this.id){
+//         return this.id;
+//         }
+//         }).get();
+
+//     $.ajax({
+//         url: form.attr("action"),
+//         type: form.attr("method"),
+//         data: form.serialize(),
+//         success: function (data) {
+//             const response = JSON.parse(data)
+//             // console.log(inputIds)
+//             if (response.success) {
+//                     // success
+//             } else {
+//                 // console.log(response.errors)
+//                 $.each(inputIds,(key,val)=>{
+
+//                     const error =  response.errors[val.split('-').slice(1).join('-')]? response.errors[val.split('-').slice(1).join('-')]:''
+//                     // console.log(response.errors[val])
+//                   if (error[0]){
+//                     $('#'+val).parent().find('.help-block').text(error)
+//                     $('#'+val).parent().find('.help-block').css('color','red')
+//                     $('#'+val).parent().find('.control-label').css('color','red')
+//                     $('#'+val).parent().find('.glyphicon').css('color','red')
+//                     $('#'+val).css('border','1px solid red')
+//                   }
+
+
+//                 })
+
+//             }
+//             // let res = JSON.parse(data)
+//             // console.log(res)
+//             // swal({
+//             //     icon: 'error',
+//             //     title: res.error,
+//             //     type: "error",
+//             //     timer: 3000,
+//             //     closeOnConfirm: false,
+//             //     closeOnCancel: false
+//             // })
+//         },
+//         error: function (data) {
+
+//         }
+//     });
+//     return false;
+// });
+// JS;
+// $this->registerJs($js);
+?>
