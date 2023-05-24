352d99c9b8fbf8d31f5e66466fd05211cc615b21
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-05-24 11:17:41 +0800

    Create m230524_004315_add_columns_in_payee_table.php

---
+++ console/migrations/m230524_004315_add_columns_in_payee_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230524_004315_add_columns_in_payee_table
+ */
+class m230524_004315_add_columns_in_payee_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('payee', 'fk_bank_id', $this->integer());
+        $this->addColumn('payee', 'account_num', $this->integer());
+        $this->createIndex('idx-pye-fk_bank_id', 'payee', 'fk_bank_id');
+        $this->addForeignKey('fk-pye-fk_bank_id', 'payee', 'fk_bank_id', 'banks', 'id', 'RESTRICT');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-pye-fk_bank_id', 'payee');
+        $this->dropIndex('idx-pye-fk_bank_id', 'payee');
+        $this->addColumn('payee', 'fk_bank_id', $this->integer());
+        $this->addColumn('payee', 'account_num', $this->integer());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230524_004315_add_columns_in_payee_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
