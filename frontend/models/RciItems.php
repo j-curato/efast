0b583d61f2edd5808f652fb2f5430be6eaaa57a5
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-06-26 10:34:00 +0800

    Update RciItems.php

--- frontend/models/RciItems.php
+++ frontend/models/RciItems.php
@@ -9,12 +9,12 @@ use Yii;
  *
  * @property int $id
  * @property int $fk_rci_id
- * @property int $fk_cash_disbursement_item_id
+ * @property int $fk_cash_disbursement_id
  * @property int $is_deleted
  * @property string|null $deleted_at
  * @property string $created_at
  *
- * @property CashDisbursementItems $fkCashDisbursementItem
+ * @property CashDisbursement $fkCashDisbursementItem
  * @property Rci $fkRci
  */
 class RciItems extends \yii\db\ActiveRecord
@@ -33,11 +33,11 @@ class RciItems extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['fk_rci_id', 'fk_cash_disbursement_item_id'], 'required'],
-            [['fk_rci_id', 'fk_cash_disbursement_item_id', 'is_deleted'], 'integer'],
+            [['fk_rci_id', 'fk_cash_disbursement_id'], 'required'],
+            [['fk_rci_id', 'fk_cash_disbursement_id', 'is_deleted'], 'integer'],
             [['deleted_at', 'created_at'], 'safe'],
-            [['fk_cash_disbursement_item_id'], 'exist', 'skipOnError' => true, 'targetClass' => CashDisbursementItems::className(), 'targetAttribute' => ['fk_cash_disbursement_item_id' => 'id']],
-            [['fk_rci_id'], 'exist', 'skipOnError' => true, 'targetClass' => Rci::className(), 'targetAttribute' => ['fk_rci_id' => 'id']],
+            [['fk_cash_disbursement_id'], 'exist', 'skipOnError' => true, 'targetClass' => CashDisbursement::class, 'targetAttribute' => ['fk_cash_disbursement_id' => 'id']],
+            [['fk_rci_id'], 'exist', 'skipOnError' => true, 'targetClass' => Rci::class, 'targetAttribute' => ['fk_rci_id' => 'id']],
         ];
     }
 
@@ -49,7 +49,7 @@ class RciItems extends \yii\db\ActiveRecord
         return [
             'id' => 'ID',
             'fk_rci_id' => 'Fk Rci ID',
-            'fk_cash_disbursement_item_id' => 'Fk Cash Disbursement Item ID',
+            'fk_cash_disbursement_id' => 'Fk Cash Disbursement Item ID',
             'is_deleted' => 'Is Deleted',
             'deleted_at' => 'Deleted At',
             'created_at' => 'Created At',
@@ -63,7 +63,7 @@ class RciItems extends \yii\db\ActiveRecord
      */
     public function getFkCashDisbursementItem()
     {
-        return $this->hasOne(CashDisbursementItems::className(), ['id' => 'fk_cash_disbursement_item_id']);
+        return $this->hasOne(CashDisbursement::class, ['id' => 'fk_cash_disbursement_id']);
     }
 
     /**
@@ -73,6 +73,6 @@ class RciItems extends \yii\db\ActiveRecord
      */
     public function getFkRci()
     {
-        return $this->hasOne(Rci::className(), ['id' => 'fk_rci_id']);
+        return $this->hasOne(Rci::class, ['id' => 'fk_rci_id']);
     }
 }
