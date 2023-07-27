88b217dc8dccfd4969c453e5ac3c9706984f432d
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-07-27 11:55:32 +0800

    Update view.php

--- frontend/views/property/view.php
+++ frontend/views/property/view.php
@@ -31,7 +31,7 @@ $qrcode_filename = Yii::$app->request->baseurl . "/frontend/views/property/qrcod
 //     'type' => 'code128',/*supported types ean8, ean13, upc, std25, int25, code11, code39, code93, code128, codabar, msi, datamatrix*/
 
 // );
-BarcodeGenerator::widget($optionsArray);
+// BarcodeGenerator::widget($optionsArray);
 ?>
 <div class="property-view">
 
