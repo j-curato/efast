6ca9669685d6c28ae9286f396a951e4d3fc53b5c
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-07-28 11:34:31 +0800

    a

---
+++ console/migrations/m230728_025244_alter_id_in_payee_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230728_025244_alter_id_in_payee_table
+ */
+class m230728_025244_alter_id_in_payee_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('payee', 'id', $this->bigInteger());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230728_025244_alter_id_in_payee_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
