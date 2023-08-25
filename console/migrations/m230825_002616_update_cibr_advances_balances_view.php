05b8cde9da317ad5d31f21ba1ca348e3069dbb87
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-08-25 08:30:14 +0800

    Update m230825_002616_update_cibr_advances_balances_view.php

--- console/migrations/m230825_002616_update_cibr_advances_balances_view.php
+++ console/migrations/m230825_002616_update_cibr_advances_balances_view.php
@@ -16,25 +16,21 @@ class m230825_002616_update_cibr_advances_balances_view extends Migration
 
             DROP VIEW IF EXISTS cibr_advances_balances;
             CREATE VIEW cibr_advances_balances as 
-                SELECT 
+                    SELECT 
                     advances.province,
                     advances.bank_account_id,
                     advances_entries.reporting_period,
                     SUM(advances_entries.amount)  as total
                     FROM advances_entries
-                    JOIN advances ON advances_entries.advances_id = advances.id
-                    JOIN dv_aucs ON advances.dv_aucs_id = dv_aucs.id
+                    LEFT JOIN advances ON advances_entries.advances_id = advances.id
                     WHERE
                     advances_entries.is_deleted NOT IN (1,9)
-                    AND dv_aucs.is_cancelled != 1
                     GROUP BY advances.province,
                     advances.bank_account_id
                     ,advances_entries.reporting_period
                     ORDER BY advances.province 
-
         SQL;
         $this->execute($sql);
-
     }
 
     /**
