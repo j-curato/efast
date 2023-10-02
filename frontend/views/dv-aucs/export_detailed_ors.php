47e868c673d4efd7603b1ffb3ba5fc0708595baa
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-10-02 14:10:20 +0800

    Create export_detailed_ors.php

---
+++ frontend/views/dv-aucs/export_detailed_ors.php
@@ -0,0 +1,104 @@
+<?php
+
+
+use kartik\date\DatePicker;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\JevPreparationSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = "RAO";
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="jev-preparation-index " style="background-color: white;padding:20px" id="main">
+    <form id="rao" @submit.prevent="exportDvs">
+        <div class="row">
+            <div class="col-sm-2">
+                <label for="year">Export Detailed DV Year</label>
+                <?php
+                echo DatePicker::widget([
+                    'name' => 'year',
+                    'id' => 'year',
+                    'pluginOptions' => [
+                        'autoclose' => true,
+                        'placeholder' => 'Select Year',
+                        'format' => 'yyyy',
+                        'minViewMode' => 'years'
+                    ]
+                ]);
+                ?>
+            </div>
+            <div class="col-sm-1" style="margin-top: 2.05rem;">
+                <button class="btn export-button" id="generate"> <i class="fas fa-file-excel text-success"></i> Export</button>
+            </div>
+            <div class="col-sm-1">
+                <div class=" center-container" v-show='loading'>
+                    <pulse-loader :loading="loading" :color="color" :size="size"></pulse-loader>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+<style>
+    .center-container {
+        margin-top: 2.05rem;
+    }
+
+    .export-button:hover {
+        background-color: #c3c7c9;
+        /* Change this to the desired hover color */
+        color: white;
+        /* Change the text color if needed */
+    }
+</style>
+<?php
+$csrf = YIi::$app->request->csrfToken;
+$this->registerJsFile('@web/js/vue-spinner.min.js', ['position' => $this::POS_HEAD]);
+?>
+<script>
+    var PulseLoader = VueSpinner.PulseLoader
+    var RingLoader = VueSpinner.RingLoader
+    $(document).ready(function() {
+        new Vue({
+            el: '#main',
+            components: {
+                'PulseLoader': PulseLoader,
+                'RingLoader': RingLoader,
+            },
+            data: {
+
+                loading: false,
+                color: '#03befc',
+                size: '20px',
+
+            },
+            computed: {},
+            methods: {
+                exportDvs() {
+                    this.loading = true
+                    this.showTable = false
+                    const url = window.location.pathname + '?r=dv-aucs/detailed-dv'
+                    const data = {
+                        year: $("#year").val(),
+                        _csrf: '<?= $csrf ?>'
+                    }
+                    const response = axios.post(url, data)
+                        .then((response) => {
+                            setTimeout(() => {
+                                this.loading = false
+                                window.open(response.data)
+                                setTimeout(() => {
+                                    this.showTable = true
+                                }, 100)
+                            }, 800)
+                        }).catch((error) => {
+                            console.log(error)
+                        })
+                },
+
+            },
+
+        })
+    })
+</script>
