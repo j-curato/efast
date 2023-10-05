352414b25252f9168d8aa81d5d77809896e7e528
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-10-05 10:11:37 +0800

    q

--- frontend/views/lddap-adas/update.php
+++ frontend/views/lddap-adas/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\LddapAdas */
 
-$this->title = 'Update Lddap Adas: ' . $model->id;
+$this->title = 'Update LDDAP ADA: ' . $model->serial_number;
 $this->params['breadcrumbs'][] = ['label' => 'Lddap Adas', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->serial_number, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="lddap-adas-update">
--- frontend/views/sliies/update.php
+++ frontend/views/sliies/update.php
@@ -5,17 +5,15 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Sliies */
 
-$this->title = 'Update Sliies: ' . $model->id;
+$this->title = 'Update SLIIE: ' . $model->serial_number;
 $this->params['breadcrumbs'][] = ['label' => 'Sliies', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->serial_number, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="sliies-update">
 
     <h3><?= Html::encode($this->title) ?></h3>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
-
 </div>
