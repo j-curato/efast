267eed7e7ad21ea2c524143cc9dece32412a584e
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-11-30 13:20:10 +0800

    Create m231130_032605_add_constraints_in_po_transmittal_entries_table.php

---
+++ console/migrations/m231130_032605_add_constraints_in_po_transmittal_entries_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231130_032605_add_constraints_in_po_transmittal_entries_table
+ */
+class m231130_032605_add_constraints_in_po_transmittal_entries_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        // $this->createIndex(
+        //     'idx-po_transmittal_entries-fk_po_transmittal_id',
+        //     'po_transmittal_entries',
+        //     'fk_po_transmittal_id'
+        // );
+        $this->addForeignKey(
+            'fk-po_transmittal_entries-fk_po_transmittal_id',
+            'po_transmittal_entries',
+            'fk_po_transmittal_id',
+            'po_transmittal',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk-po_transmittal_entries-fk_po_transmittal_id', 'po_transmittal_entries');
+        $this->dropIndex('idx-po_transmittal_entries-fk_po_transmittal_id', 'po_transmittal_entries');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m231130_032605_add_constraints_in_po_transmittal_entries_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
