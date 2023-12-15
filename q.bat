cf11ec1119f221b187ebe8fb32c54b6d4099174d
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-12-15 16:36:23 +0800

    Update q.bat

--- q.bat
+++ q.bat
@@ -1,14 +1,3 @@
-@echo off
-
-rem Get the path of the batch file's directory
 set "batchDir=%~dp0"
-
-rem Change to the batch file's directory
-cd /d "%batchDir%"
-
-rem Perform git fetch and git pull
-git fetch
-git pull
-
-rem Pause (optional) to keep the console window open
-pause
+cd  /d "%batchDir%" && git fetch
+cd  /d "%batchDir%" && git pull
