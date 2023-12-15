2d880c666a88864a6175885ab6ffd5df826b6919
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-12-15 15:09:13 +0800

    Create gitpull.sh

---
+++ gitpull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if the script is run from the correct directory
+if [ ! -f "composer.json" ]; then
+    echo "Error: This script must be run from the Yii2 project root directory."
+    exit 1
+fi
+
+git fetch
+# Perform git pull
+git pull 
+
+# Install/update Composer dependencies
+# composer install --no-interaction --optimize-autoloader
+
+# Run any necessary database migrations
+php yii migrate --interactive=0
