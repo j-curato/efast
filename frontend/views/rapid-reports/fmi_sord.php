015ee8b88c23b5ee3a4c89bd156927738da90d89
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-12-15 16:39:05 +0800

    Update fmi_sord.php

--- frontend/views/rapid-reports/fmi_sord.php
+++ frontend/views/rapid-reports/fmi_sord.php
@@ -8,7 +8,7 @@ use yii\helpers\ArrayHelper;
 
 $this->title = "FMI SORD";
 $this->params['breadcrumbs'][] = $this->title;
-echo "ssssssssssss";
+echo "ddddddddddd";
 ?>
 <div class="jev-preparation-index" style="background-color: white;" id="mainVue">
 
