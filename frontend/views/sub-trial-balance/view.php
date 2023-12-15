3228e6d8554db2e948c76309eac756175309e503
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-12-15 14:07:25 +0800

    Update view.php

--- frontend/views/sub-trial-balance/view.php
+++ frontend/views/sub-trial-balance/view.php
@@ -106,12 +106,12 @@ $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' 
 
 
     </div>
-    <div id="dots5">
+    <!-- <div id="dots5">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-    </div>
+    </div> -->
 
 
 </div>
