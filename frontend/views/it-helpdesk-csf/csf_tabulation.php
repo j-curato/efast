73ceb587c5f78a27b898f028e24d62883abe056a
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2023-12-29 10:39:14 +0800

    Update csf_tabulation.php

--- frontend/views/it-helpdesk-csf/csf_tabulation.php
+++ frontend/views/it-helpdesk-csf/csf_tabulation.php
@@ -56,11 +56,11 @@ $this->params['breadcrumbs'][] = $this->title;
             <table>
                 <tr>
                     <th>Period </th>
-                    <th colspan="4"></th>
+                    <th colspan="6"></th>
                 </tr>
                 <tr>
                     <th>No. of Raters</th>
-                    <th class="text-center " colspan="4">{{csf.details.number_of_raters}}</th>
+                    <th class="text-center " colspan="6">{{csf.details.number_of_raters}}</th>
                 </tr>
                 <tr>
                     <th></th>
@@ -68,13 +68,23 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th>SATISFIED</th>
                     <th>DISSATISFIED </th>
                     <th>VERY DISSATISFIED</th>
+                    <th>CSF Score</th>
+                    <th>CSF Rating</th>
                 </tr>
                 <tr>
                     <th>Clarity</th>
-                    <th class="text-center"> {{getRates(4,'clarity')}}</th>
+                    <th class="text-center"> {{getRates(4,'clarity')}}
+
+                    </th>
                     <th class="text-center"> {{getRates(3,'clarity')}}</th>
                     <th class="text-center"> {{getRates(2,'clarity')}}</th>
                     <th class="text-center"> {{getRates(1,'clarity')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('clarity')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('clarity')}}%
+                    </th>
                 </tr>
                 <tr>
                     <th>Skills</th>
@@ -82,6 +92,12 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th class="text-center"> {{getRates(3,'skills')}}</th>
                     <th class="text-center"> {{getRates(2,'skills')}}</th>
                     <th class="text-center"> {{getRates(1,'skills')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('skills')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('skills')}}%
+                    </th>
                 </tr>
                 <tr>
                     <th>Professionalism</th>
@@ -89,6 +105,12 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th class="text-center"> {{getRates(3,'professionalism')}}</th>
                     <th class="text-center"> {{getRates(2,'professionalism')}}</th>
                     <th class="text-center"> {{getRates(1,'professionalism')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('professionalism')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('professionalism')}}%
+                    </th>
                 </tr>
                 <tr>
                     <th>Courtesy</th>
@@ -96,6 +118,12 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th class="text-center"> {{getRates(3,'courtesy')}}</th>
                     <th class="text-center"> {{getRates(2,'courtesy')}}</th>
                     <th class="text-center"> {{getRates(1,'courtesy')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('courtesy')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('courtesy')}}%
+                    </th>
                 </tr>
                 <tr>
                     <th>Response Time</th>
@@ -103,6 +131,12 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th class="text-center"> {{getRates(3,'response_time')}}</th>
                     <th class="text-center"> {{getRates(2,'response_time')}}</th>
                     <th class="text-center"> {{getRates(1,'response_time')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('response_time')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('response_time')}}%
+                    </th>
                 </tr>
                 <tr>
                     <th>OUTCOME/Result of Services Requested</th>
@@ -110,13 +144,81 @@ $this->params['breadcrumbs'][] = $this->title;
                     <th class="text-center"> {{getRates(3,'outcome')}}</th>
                     <th class="text-center"> {{getRates(2,'outcome')}}</th>
                     <th class="text-center"> {{getRates(1,'outcome')}}</th>
+                    <th class="text-center">
+                        {{computeCsfScore('outcome')}}
+                    </th>
+                    <th class="text-center">
+                        {{computeCsfRating('outcome')}}%
+                    </th>
                 </tr>
 
-
-
-
-
             </table>
+            <table class="mt-3 ">
+
+                <tr>
+                    <th colspan="2">SQD (OVERALL SCORE)</th>
+
+                </tr>
+                <tr>
+                    <th class="text-center">Dimensions </th>
+                    <th class="text-center">CSF Score</th>
+                </tr>
+                <tr>
+                    <th>Clarity</th>
+                    <th class="text-center"> {{computeCsfScore('clarity')}}</th>
+
+                </tr>
+                <tr>
+                    <th>Skills</th>
+
+                    <th class="text-center"> {{computeCsfScore('skills')}}</th>
+
+                </tr>
+                <tr>
+                    <th>Professionalism</th>
+                    <th class="text-center"> {{computeCsfScore('professionalism')}}</th>
+
+                </tr>
+                <tr>
+                    <th>Courtesy</th>
+                    <th class="text-center"> {{computeCsfScore('courtesy')}}</th>
+
+                </tr>
+                <tr>
+                    <th>Response Time</th>
+                    <th class="text-center"> {{computeCsfScore('response_time')}}</th>
+
+                </tr>
+                <tr>
+                    <th>OUTCOME/Result of Services Requested</th>
+                    <th class="text-center"> {{computeCsfScore('outcome')}}</th>
+
+                </tr>
+                <tr>
+                    <th>OVERALL AVE</th>
+                    <th class="text-center">{{computeCsfScoreAve}}</th>
+                </tr>
+                <tr>
+                    <th>OVERALL RATING</th>
+                    <th class="text-center">{{computeCsfRatingAve}}</th>
+                </tr>
+            </table>
+            <p class="mt-4">
+                For period ________,
+                a total of <b> {{csf.details.number_of_raters}}</b> clients received a CSF in relation to the
+                IT Technical Assistance service process.
+                The process owner successfully collected all the forms,
+                demonstrating 100% retrieval efficiency across both online and offline platforms.
+                An evaluation of the overall process revealed a weighted average rating of <b>
+                    {{computeCsfScoreAve}}</b>,
+                translating into a CSF score of <b> {{computeCsfRatingAve}}%</b>.
+                This score is calculated based on the SQD of the process,
+                reflecting a Satisfied level of client satisfaction.
+
+
+
+
+            </p>
         </div>
     </div>
 
@@ -171,11 +273,48 @@ $this->params['breadcrumbs'][] = $this->title;
                 if (filteredRateType) {
                     return filteredRateType['num_of_rates']
                 }
+                return 0;
 
-            }
+            },
+            computeCsfScore(type) {
+
+                let res = ((this.getRates(4, type) * 4) +
+                        (this.getRates(3, type) * 3) +
+                        (this.getRates(2, type) * 2) +
+                        (this.getRates(1, type) * 1)) /
+                    parseInt(this.csf.details.number_of_raters)
+                return isNaN(res) ? 0 : res.toFixed(2)
+            },
+            computeCsfRating(type) {
+                return (this.computeCsfScore(type) / 4) * 100
+            },
+
 
         },
         computed: {
+            computeCsfScoreAve() {
+
+                let ave =
+                    (parseFloat(this.computeCsfScore('clarity')) +
+                        parseFloat(this.computeCsfScore('skills')) +
+                        parseFloat(this.computeCsfScore('professionalism')) +
+                        parseFloat(this.computeCsfScore('courtesy')) +
+                        parseFloat(this.computeCsfScore('response_time')) +
+                        parseFloat(this.computeCsfScore('outcome'))
+                    ) / 6
+
+                return ave.toFixed(2)
+            },
+            computeCsfRatingAve() {
+
+                const ave = (this.computeCsfRating('clarity') +
+                    this.computeCsfRating('skills') +
+                    this.computeCsfRating('professionalism') +
+                    this.computeCsfRating('courtesy') +
+                    this.computeCsfRating('response_time') +
+                    this.computeCsfRating('outcome')) / 6
+                return ave.toFixed(2)
+            },
 
         }
 
