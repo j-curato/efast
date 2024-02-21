418a4e87d5fd56fe7be43995e299d86bf358205c
kiotipot1 <74756933+kiotipot1@users.noreply.github.com>
2024-02-21 15:07:39 +0800

    Revert "Update view.php"
    This reverts commit 7632aebb8789b625ecf6a0bb0c230f91517b461f.

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,64 +1,35 @@
 <?php
-
-use yii\symfonymailer\Mailer;
-
-
-
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.1.172;dbname=efast',
-            'username' => 'dbadmin2',
-            'password' => 'password',
-            'charset' => 'utf8',
-        ],
-        'cloud_db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=163.44.242.9;dbname=vxioebfv_sample',
-            'username' => 'vxioebfv_kiotipot',
-            'password' => 'fuckthis.l.',
-            'charset' => 'utf8',
-        ],
-        'ryn_db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=10.20.17.26;dbname=afms',
-            'username' => 'user1',
-            'password' => 'password',
-            'charset' => 'utf8',
-        ],
-        'afms_dev' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=afms_dev',
+            'dsn' => 'mysql:host=localhost; dbname=afms',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
         ],
-        'restore_db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=restore',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => Mailer::class,
-            'transport' => [
-                // 'class' => 'Swift_SmtpTransport',
-                'scheme' => 'smtps',
-                'host' => 'smtp.gmail.com',
-                'username' => 'norman.notorious@gmail.com',
-                'password' => 'nkifgkkrewukufhd',
-                'port' => 465,
-                // 'dsn' => 'native://default',
-                'encryption' => 'tls'
-            ],
-            'viewPath' => '@frontend/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure transport
-            // for the mailer to send real emails.
-            'useFileTransport' => false,
-        ],
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=dtilocalserver;dbname=efast',
+        //     'username' => 'dbadmin2',
+        //     'password' => 'password',
+        //     'charset' => 'utf8',
+        // ],
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'mysql:host=db-efast.cur0n7vldhz5.ap-southeast-1.rds.amazonaws.com;dbname=efast',
+        //     'username' => 'efastadmin',
+        //     'password' => 'Dticaraga13',
+        //     'charset' => 'utf8',
+        // ],
 
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
     ],
 ];
